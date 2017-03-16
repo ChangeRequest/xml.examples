a7c36a8482e0777f3612dff24e5fb70e9b077ef2
dmytro.mudrov <dmytro.mudrov@gmail.com>
2017-03-16 21:54:16 +0000

    Updated example to be more clear.

--- src/main/java/school/lemon/changerequest/java/xml/examples/DomParserExample.java
+++ src/main/java/school/lemon/changerequest/java/xml/examples/DomParserExample.java
@@ -8,6 +8,8 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.io.IOException;
+
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,17 +18,20 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.IOException;
 
 public class DomParserExample {
+
     private static final String TEST_XML = "complexTypesTnsExample.xml";
     private static final String TEST_XSD = "complexTypesTnsExample.xsd";
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(Thread.currentThread().getContextClassLoader().getResource(TEST_XSD));
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setNamespaceAware(true);
+        dbf.setIgnoringElementContentWhitespace(true);
+        dbf.setSchema(schema);
         DocumentBuilder db = dbf.newDocumentBuilder();
-
         db.setErrorHandler(new DefaultHandler() {
             @Override
             public void error(SAXParseException e) throws SAXException {
@@ -34,16 +39,23 @@ public class DomParserExample {
             }
         });
         Document document = db.parse(Thread.currentThread().getContextClassLoader().getResourceAsStream(TEST_XML));
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema = schemaFactory.newSchema(Thread.currentThread().getContextClassLoader().getResource(TEST_XSD));
         Validator validator = schema.newValidator();
         validator.validate(new DOMSource(document));
         Element rootElement = document.getDocumentElement();
         System.out.println("Root tag is: " + rootElement.getTagName());
-        NodeList childNodes = rootElement.getChildNodes();
-        for (int i = 0; i < childNodes.getLength(); i++) {
-            Node child = childNodes.item(i);
-            System.out.println(String.format("%d child tag name: %s", i + 1, child.getNodeName()));
+        printNodes("", rootElement.getChildNodes());
+    }
+
+    private static void printNodes(String indent, NodeList nodes) {
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node child = nodes.item(i);
+            if (child.hasChildNodes()) {
+                System.out.println(String.format("%s %s:", indent, child.getNodeName()));
+                printNodes(indent + " ", child.getChildNodes());
+            } else {
+                System.out.println(String.format("%s %s", indent, child.getNodeValue()));
+            }
         }
     }
+
 }
